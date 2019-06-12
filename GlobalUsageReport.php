c87249ecc3bac9cd1c35291dbfb8d8aca24da490
Andrew Martin <andy123@stanford.edu>
2019-06-12 16:11:57 -0700

    adding report metrics

--- GlobalUsageReport.php
+++ GlobalUsageReport.php
@@ -6,10 +6,15 @@ require APP_PATH_DOCROOT . "ControlCenter/header.php";
 
 $module->emDebug("---- Global Usage Report -----");
 
+
+echo $module->getUrl("api/metrics.php",true,true);
+
 ?>
 
 <h3><? echo $module->getModuleName() ?></h3>
 <h4> Global Usage Report </h4>
+
+
 
 
 <style>
---
+++ api/metrics.php
@@ -0,0 +1,74 @@
+<?php
+namespace Stanford\UserProjectSessions;
+/** @var UserProjectSessions $module */
+
+
+/**
+ * This is called as an ajax services from the google data studio to report metrics on REDCap
+ */
+
+
+$shared_secret_token = $module->getSystemSetting('shared_secret_token');
+$token = @$_GET['token'];
+
+if (empty($shared_secret_token)) {
+    exit("Security token not configured");
+}
+
+if (empty($token)) {
+    $module->emDebug("Missing token");
+    exit("Improper API request");
+}
+
+if ($token != $shared_secret_token) {
+    $module->emDebug("Invalid token: $token");
+    exit("Improper authentication - this has been logged.");
+}
+
+
+$report = @$_GET['report'];
+
+switch($report) {
+    case "userProjectSessions":
+
+        $sql = "select * from em_user_project_sessions";
+        $q = db_query($sql);
+        $data = [];
+        while ($row = db_fetch_assoc($q)) {
+            $data[] = $row;
+        }
+        break;
+
+    case "projects":
+        $sql = '
+select
+  CASE
+    WHEN status = 0 THEN "Development"
+    WHEN status = 1 THEN "Production"
+    WHEN status = 2 THEN "Inactive"
+    WHEN status = 3 THEN "Archived"
+    ELSE NULL
+  END as Status,
+  Count
+from
+     (select status,
+             count(*) as Count
+      from redcap_projects
+      group by status
+     ) Raw';
+        $q = db_query($sql);
+        $data = [];
+        while ($row = db_fetch_assoc($q)) {
+            $data[] = $row;
+        }
+        break;
+
+
+    default:
+        $module->emDebug("Invalid Report Option: " . $report);
+}
+
+if (!empty($data)) {
+    header("Content-Type: application/json");
+    echo json_encode($data);
+}
